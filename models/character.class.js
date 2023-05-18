6eacf2dcb8cbc161896bccb0dfacaf2e4c52f376
Neesou <93050748+Neesou@users.noreply.github.com>
2023-05-18 17:00:22 +0200

    add endboss and some finetuning

--- models/character.class.js
+++ models/character.class.js
@@ -5,6 +5,8 @@ class Character extends MovebleObject {
     y = 160
     speed = 5
 
+    idleTimer = 0;
+
 
 
     IMAGES_WALKING = [
@@ -16,13 +18,41 @@ class Character extends MovebleObject {
         'img/2_character_pepe/2_walk/W-26.png'
     ]
 
+    IMAGES_IDLE = [
+        'img/2_character_pepe/1_idle/idle/I-1.png',
+        'img/2_character_pepe/1_idle/idle/I-2.png',
+        'img/2_character_pepe/1_idle/idle/I-3.png',
+        'img/2_character_pepe/1_idle/idle/I-4.png',
+        'img/2_character_pepe/1_idle/idle/I-5.png',
+        'img/2_character_pepe/1_idle/idle/I-6.png',
+        'img/2_character_pepe/1_idle/idle/I-7.png',
+        'img/2_character_pepe/1_idle/idle/I-8.png',
+        'img/2_character_pepe/1_idle/idle/I-9.png',
+        'img/2_character_pepe/1_idle/idle/I-10.png'
+    ]
+
+    IMAGES_IDLE_LONG = [
+        'img/2_character_pepe/1_idle/long_idle/I-11.png',
+        'img/2_character_pepe/1_idle/long_idle/I-12.png',
+        'img/2_character_pepe/1_idle/long_idle/I-13.png',
+        'img/2_character_pepe/1_idle/long_idle/I-14.png',
+        'img/2_character_pepe/1_idle/long_idle/I-15.png',
+        'img/2_character_pepe/1_idle/long_idle/I-16.png',
+        'img/2_character_pepe/1_idle/long_idle/I-17.png',
+        'img/2_character_pepe/1_idle/long_idle/I-18.png',
+        'img/2_character_pepe/1_idle/long_idle/I-19.png',
+        'img/2_character_pepe/1_idle/long_idle/I-20.png'
+    ]
+
     walking_sound = new Audio('audio/walkingSand.mp3');
 
     currentImage = 0;
     world;
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages(this.IMAGES_WALKING)
+        this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_IDLE);
+        this.loadImages(this.IMAGES_IDLE_LONG);
         this.animate()
 
     }
@@ -33,33 +63,51 @@ class Character extends MovebleObject {
 
     animate() {
         setInterval(() => {
-
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.playAnimation(this.IMAGES_WALKING);
+                this.idleTimer = 0;
             }
-
             if (this.world.keyboard.LEFT && this.x > 20) {
                 this.playAnimation(this.IMAGES_WALKING);
+                this.idleTimer = 0;
             }
 
         }, 1000 / 10);
-
-        window.addEventListener('keyup', (event) => {this.walking_sound.pause();})
 
         setInterval(() => {
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
-                this.walking_sound.play();
             }
 
             if (this.world.keyboard.LEFT && this.x > 20) {
                 this.x -= this.speed;
                 this.otherDirection = true;
-                this.walking_sound.play();
             }
             this.world.camera_x = -this.x + 50;
         }, 1000 / 60);
+
+        setInterval(() => {
+
+            if(this.idleTimer > 0.5){
+                if (!this.world.keyboard.RIGHT && this.idleTimer < 300) {
+                    this.playAnimation(this.IMAGES_IDLE);
+                    this.idleTimer ++;
+                }
+    
+                if (!this.world.keyboard.LEFT && this.idleTimer < 300) {
+                    this.playAnimation(this.IMAGES_IDLE);
+                    this.idleTimer ++;
+                }
+    
+                if (this.idleTimer >= 300) {
+                    this.playAnimation(this.IMAGES_IDLE_LONG);
+                }
+            }            
+
+            this.idleTimer ++;
+
+        }, 1000 / 5);
 
     }
 }
